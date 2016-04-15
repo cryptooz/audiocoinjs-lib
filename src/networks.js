7f1473b765a6b65e317cbb01531dcb62e09da66e
jointhepartypooper <wongair@WongAir.local>
2016-04-15 17:14:38 +0200

    just peercoin network

--- src/networks.js
+++ src/networks.js
@@ -2,37 +2,15 @@
 // Dogecoin BIP32 is a proposed standard: https://bitcointalk.org/index.php?topic=409731
 
 module.exports = {
-  bitcoin: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4
-    },
-    pubKeyHash: 0x00,
-    scriptHash: 0x05,
-    wif: 0x80,
-    dustThreshold: 546 // https://github.com/bitcoin/bitcoin/blob/v0.9.2/src/core.h#L151-L162
-  },
-  testnet: {
-    messagePrefix: '\x18Bitcoin Signed Message:\n',
-    bip32: {
-      public: 0x043587cf,
-      private: 0x04358394
-    },
-    pubKeyHash: 0x6f,
-    scriptHash: 0xc4,
-    wif: 0xef,
-    dustThreshold: 546
-  }, 
   peercoin: {
-    messagePrefix: '\x19Peercoin Signed Message:\n',
+    messagePrefix: '\x17PPcoin Signed Message:\n',
     bip32: {
-      public: 0x0488b21e,
-      private: 0x0488ade4 // ??
+      public: 0x01da950b,  // start with "Ppub..."
+      private: 0x01da90d0  // start with "Pprv..."
     },
     pubKeyHash: 0x37,
-    scriptHash: 0x05,
+    scriptHash: 0x75,
     wif: 0xb7,
-    dustThreshold: 0 // To limit dust spam, require MIN_TX_FEE/MIN_RELAY_TX_FEE if any output is less than 0.01
+    dustThreshold: 0  
   }
 }
